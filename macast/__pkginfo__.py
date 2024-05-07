bb8c3649d91db86fcc07e20955dfdf763748c717
liwen15 <liwen15@staff.sina.com.cn>
2024-05-07 14:08:11 +0800

    调整github action

--- macast/__pkginfo__.py
+++ macast/__pkginfo__.py
@@ -1,8 +1,8 @@
 __version_num__ = 0.7
 __version__ = f'{int(__version_num__)}.{int(__version_num__ * 10 % 10)}.{int(__version_num__ * 100 % 10)}'
 __url__ = 'https://github.com/xfangfang/Macast'
-__author__ = 'xfangfang'
-__email__ = 'xfangfang@126.com'
+__author__ = 'liwen15'
+__email__ = 'liwen15'
 __license__ = 'GPL3'
-__name__ = 'macast'
-__description__ = 'a DLNA Media Renderer'
+__name__ = 'SinaCast'
+__description__ = '投屏接收器'
